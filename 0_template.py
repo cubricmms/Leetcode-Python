d18edb6e0827578e7060ffc2550ff4d85003db62
cubricmms <cubric.mms@gmail.com>
2021-09-12 22:05:12 +0800

    Add boilerplate for quick initiation

---
+++ 0_template.py
@@ -0,0 +1,12 @@
+# Leetcode problem x (xxx): https://leetcode.com/problems/xxxx/
+# Level: xxx
+
+
+class Solution:
+    def xxx(self, s: str) -> int:
+        pass
+
+
+if __name__ == "__main__":
+    solution = Solution()
+    assert solution.xxx(x=123) == 321
